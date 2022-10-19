08bb2c7918e73c3cd2a04677ad404d165a8c35ef
doducuy <doducuyktdn@gmail.com>
2022-10-19 10:53:26 +0700

    nop bai

--- ss_3_thymeleaf/exercise/product_management/src/main/java/com/codegym/contronller/ProductController.java
+++ ss_3_thymeleaf/exercise/product_management/src/main/java/com/codegym/contronller/ProductController.java
@@ -1,8 +1,8 @@
 package com.codegym.contronller;
 
 import com.codegym.model.Product;
-import com.codegym.service.ProductService;
 import com.codegym.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,17 +13,18 @@ import java.util.List;
 @Controller
 @RequestMapping("/product")
 public class ProductController {
-    private final IProductService productService = new ProductService();
+    @Autowired
+    private IProductService productService ;
     @GetMapping("")
     public String index(Model model) {
         List<Product> productList = productService.findAll();
         model.addAttribute("products", productList);
-        return "/index";
+        return "product/index";
     }
     @GetMapping("/create")
     public String create(Model model){
         model.addAttribute("product",new Product());
-        return "/create";
+        return "product/create";
     }
     @PostMapping("/save")
     public String save(@ModelAttribute(value = "product") Product product, RedirectAttributes redirectAttributes){
@@ -35,7 +36,7 @@ public class ProductController {
     @GetMapping("/{id}/delete")
     public String delete(@PathVariable(value = "id") int id,Model model){
         model.addAttribute("product", productService.findById(id));
-        return "/delete";
+        return "product/delete";
     }
     @PostMapping("/delete")
     public String delete(Product product, RedirectAttributes redirect) {
@@ -46,7 +47,7 @@ public class ProductController {
     @GetMapping("/{id}/edit")
     public String edit(@PathVariable int id, Model model) {
         model.addAttribute("product", productService.findById(id));
-        return "/edit";
+        return "product/edit";
     }
     @PostMapping("/update")
     public String update(Product product) {
@@ -56,11 +57,11 @@ public class ProductController {
     @GetMapping("/{id}/view")
     public String view(@PathVariable int id, Model model) {
         model.addAttribute("product", productService.findById(id));
-        return "/view";
+        return "product/view";
     }
     @GetMapping("/search")
-    public String search(@RequestParam(value = "searchName") String name, Model model ){
+    public String search(@RequestParam(value = "searchName", defaultValue = "  ") String name, Model model ){
         model.addAttribute("products",productService.findByName(name));
-        return "/index";
+        return "product/index";
     }
 }
