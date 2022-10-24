104180f0fa59ddc6b8249ce7c45d8a6a606e36cd
doducuy <doducuyktdn@gmail.com>
2022-10-24 21:06:27 +0700

    nopbai

--- ss_8_validationt/exercise/song/src/main/java/com/example/demo/controller/SongController.java
+++ ss_8_validationt/exercise/song/src/main/java/com/example/demo/controller/SongController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Song;
+import com.example.demo.model.SongDto;
 import com.example.demo.service.ISongService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class SongController {
@@ -20,7 +27,21 @@ public class SongController {
     }
     @GetMapping("/create")
     public String showCreateForm(Model model){
-        model.addAttribute("song",new Song());
+        model.addAttribute("songDto",new SongDto());
         return "/create";
+    }
+    @PostMapping("saveSong")
+    public String saveUser(@Validated @ModelAttribute(value = "songDto") SongDto songDto, BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes){
+        new SongDto().validate(songDto,bindingResult);
+        if(bindingResult.hasErrors()){
+            return "/create";
+        }else {
+            Song song = new Song();
+            BeanUtils.copyProperties(songDto,song);
+            iSongService.saveSong(song);
+            redirectAttributes.addFlashAttribute("mess", "Add Success!!!");
+            return "redirect:/list";
+        }
     }
 }
